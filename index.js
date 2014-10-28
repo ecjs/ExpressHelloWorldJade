c2db7d2c08f9c3be1acde0271c11354b03ebe69c
Mark Harrell <mark@mjharrell.com>
2014-10-28 11:02:52 -0700

    first commit for hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+// declare express, app as express and use env port or 3000
+var express = require("express"), app = express(), port = process.env.PORT || 3000;
+// set the view engine to jade
+app.set('view engine', 'jade');
+// set the static resource directory to /public
+app.use(express.static(__dirname + '/public'));
+// set the main route, rendering using jade
+app.get("/", function(req, res) {
+  res.render('index', { title: 'Node/Express/Jade Hello World!', message: 'Node/Express/Jade Hello World!', email: 'mark@mjharrell.com', twitter: 'mark_harrell'});
+});
+
+//start the server
+var server = app.listen(3000, function (req,res) {
+  var listening = server.address().port;
+  console.log("Now listening on port " + listening);
+});
